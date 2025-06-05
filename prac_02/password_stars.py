e54f60fb61dd2e65e26b4db4eb248affc9e9b5a7
JeremyJCU <jeremy.wiseman@my.jcu.edu.au>
2025-06-05 12:29:31 +1000

    Refactor password check to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,24 @@
-"""Valid password check and length display using star character"""
+"""Practical 02 password program"""
 PASSWORD_LENGTH = 10
-user_password = input("Enter your password: ")
-while len(user_password) < PASSWORD_LENGTH:
-    print(f"Password must be at least {PASSWORD_LENGTH} characters long")
+def main():
+    """Get a password and display representation using star character"""
+    password = get_password()
+    display_password(password)
+
+
+def display_password(password):
+    """Display the password using star character"""
+    print("*" * len(password))
+
+
+def get_password():
+    """Get a valid password from the user"""
     user_password = input("Enter your password: ")
-print("*" * len(user_password))
+    while len(user_password) < PASSWORD_LENGTH:
+        print(f"Password must be at least {PASSWORD_LENGTH} characters long")
+        user_password = input("Enter your password: ")
+    return user_password
+
+
+main()
 
