c0187265d634cd853ea0ef9943672b1101398bdf
JeremyJCU <jeremy.wiseman@my.jcu.edu.au>
2025-06-05 13:32:33 +1000

    Determine_result function added and random_score generated.

--- prac_02/score.py
+++ prac_02/score.py
@@ -7,17 +7,27 @@ Remember to click Raw before copying and pasting so that you get proper formatti
 
 The intention is that the score must be between 0 and 100 inclusive; 90 or more is excellent; 50 or more is a pass; below 50 is bad. There is no intention to do any repetition.
 """
+from random import randint
 
-# TODO: Fix this!
+def main():
+    """Take score and print determined result."""
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print("Human entered: ", result)
+    random_score = float(randint(0, 100))
+    result = determine_result(random_score)
+    print(f"Computer generated result: {result} from score: {random_score} ")
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad score")
-elif 90 > score >= 50:
-    print("Passable")
-else:
-    print("Excellent")
+def determine_result(score):
+    """Determine score result."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad score"
+    elif 90 > score >= 50:
+        return "Passable"
+    else:
+        return "Excellent"
 
 
+main()
