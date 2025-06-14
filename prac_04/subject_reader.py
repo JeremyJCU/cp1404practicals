e8cc1f0bed5e57d58988a22fd2d29f1f0cc346ce
JeremyJCU <jeremy.wiseman@my.jcu.edu.au>
2025-06-14 21:35:24 +1000

    File lines added to subject_list which is then printed using formatted data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,13 +7,21 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Loads data from file and prints report."""
     data = load_data()
-    print(data)
+    print_subject_details(data)
+
+
+def print_subject_details(data):
+    """Prints subject details."""
+    for subject in data:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +31,9 @@ def load_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        subject_data.append(parts)
     input_file.close()
+    return subject_data
 
 
 main()
